0554fc23dd59cc67a5b7710354af361033ded0d8
Sebastian Bischlager <sebastian-bischlager@web.de>
2017-12-19 21:27:12 +0100

    Made library header only

--- src/cpuid/cpuinfo.h
+++ src/cpuid/cpuinfo.h
@@ -3,7 +3,20 @@
 //
 // Distributed under the "BSD License". See the accompanying LICENSE.rst file.
 
-#include <platform/config.hpp>
+
+#if defined(_WIN32)
+    #if defined(_WIN64) 
+    #error Windows x64 not supported
+    #else
+        #if defined(_MSC_VER)
+        #define PLATFORM_MSVC_X86
+        #endif
+        
+        #if defined(__GNUC__)
+        #define PLATFORM_GCC_COMPATIBLE_X86
+        #endif
+    #endif
+#endif
 
 #include "cpuinfo.hpp"
 #include "cpuinfo_impl.hpp"
